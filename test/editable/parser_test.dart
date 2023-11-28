6fda9ac0d6a45ebdbed21888c3abd7bc152bbf0a
Marc Renken <marc.renken@bitbot.eu>
2023-11-28 14:23:48 +0100

    test(GeneratedParser): borderless text to Capsule/Random

--- test/editable/parser_test.dart
+++ test/editable/parser_test.dart
@@ -1,38 +1,35 @@
 import 'package:test/test.dart';
 import 'package:text_gen/text_gen.dart';
 
+void parseAndReparse(String input, EditableGen expected) {
+  final parsed = GeneratedParser.parse(input);
+  expect(parsed, equals(expected));
+
+  final rebuild = parsed?.buildArguments() ?? '';
+  final reParsed = GeneratedParser.parse(rebuild);
+  expect(reParsed, equals(expected));
+  expect(reParsed.toString(), equals(expected.toString()));
+  expect(reParsed.hashCode, equals(expected.hashCode));
+}
+
 void main() {
   group('GeneratedParser', () {
     test('should parse simple text', () {
       const input = 'Hello';
-      final expected = EditableCapsule(encapsulated: [EditableTxt(text: input)]);
-      final parsed = GeneratedParser.parse(input);
-      expect(parsed, equals(expected));
-      expect(parsed.toString(), equals(expected.toString()));
-      expect(parsed?.buildArguments(), equals('($input)'));
-
-      final rebuild = parsed?.buildArguments() ?? '';
-      final reParsed = GeneratedParser.parse(rebuild);
-      expect(reParsed, equals(expected));
-      expect(reParsed.toString(), equals(expected.toString()));
-      expect(reParsed.hashCode, equals(expected.hashCode));
+      final expected =
+          EditableCapsule(encapsulated: [EditableTxt(text: input)]);
+      parseAndReparse(input, expected);
     });
 
     test('should parse encapsulated text', () {
       const input = '(Hello, world!)';
       final expected = EditableCapsule(
-        encapsulated: [EditableTxt(text: 'Hello,'), EditableTxt(text: 'world!')],
+        encapsulated: [
+          EditableTxt(text: 'Hello,'),
+          EditableTxt(text: 'world!')
+        ],
       );
-      final parsed = GeneratedParser.parse(input);
-      expect(parsed, equals(expected));
-      expect(parsed.toString(), equals(expected.toString()));
-      expect(parsed?.buildArguments(), equals(input));
-
-      final rebuild = parsed?.buildArguments() ?? '';
-      final reParsed = GeneratedParser.parse(rebuild);
-      expect(reParsed, equals(expected));
-      expect(reParsed.toString(), equals(expected.toString()));
-      expect(reParsed.hashCode, equals(expected.hashCode));
+      parseAndReparse(input, expected);
     });
 
     test('should parse nested encapsulated text', () {
@@ -44,37 +41,47 @@ void main() {
           EditableTxt(text: 'world!'),
         ],
       );
-      final parsed = GeneratedParser.parse(input);
-      expect(parsed, equals(expected));
-      expect(parsed.toString(), equals(expected.toString()));
-      expect(parsed?.buildArguments(), equals(input));
-
-      final rebuild = parsed?.buildArguments() ?? '';
-      final reParsed = GeneratedParser.parse(rebuild);
-      expect(reParsed, equals(expected));
-      expect(reParsed.toString(), equals(expected.toString()));
-      expect(reParsed.hashCode, equals(expected.hashCode));
+      parseAndReparse(input, expected);
     });
 
-    test('should parse random text', () {
-      const input = '{Hello Hi}, {world universe}!';
+    test('should parse text borderless to encapsulated', () {
+      const input = 'close(Hello, world!)';
       final expected = EditableCapsule(
         encapsulated: [
-          EditableRandom(possibilities: [EditableTxt(text: 'Hello'), EditableTxt(text: 'Hi')]),
+          EditableTxt(text: 'close'),
+          EditableCapsule(
+            encapsulated: [
+              EditableTxt(text: 'Hello,'),
+              EditableTxt(text: 'world!'),
+            ],
+          ),
+        ],
+      );
+      parseAndReparse(input, expected);
+    });
+
+    test('should parse text borderless to random', () {
+      const input = '{Hello Hi}, close{world universe}!';
+      final expected = EditableCapsule(
+        encapsulated: [
+          EditableRandom(
+            possibilities: [
+              EditableTxt(text: 'Hello'),
+              EditableTxt(text: 'Hi'),
+            ],
+          ),
           EditableTxt(text: ','),
-          EditableRandom(possibilities: [EditableTxt(text: 'world'), EditableTxt(text: 'universe')]),
+          EditableTxt(text: 'close'),
+          EditableRandom(
+            possibilities: [
+              EditableTxt(text: 'world'),
+              EditableTxt(text: 'universe'),
+            ],
+          ),
           EditableTxt(text: '!'),
         ],
       );
-      final parsed = GeneratedParser.parse(input);
-      expect(parsed, equals(expected));
-      expect(parsed.toString(), equals(expected.toString()));
-
-      final rebuild = parsed?.buildArguments() ?? '';
-      final reParsed = GeneratedParser.parse(rebuild);
-      expect(reParsed, equals(expected));
-      expect(reParsed.toString(), equals(expected.toString()));
-      expect(reParsed.hashCode, equals(expected.hashCode));
+      parseAndReparse(input, expected);
     });
   });
 }
