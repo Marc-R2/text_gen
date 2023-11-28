7264ffa5ebf763fd33f6c4983b0494536692102d
Marc Renken <marc.renken@bitbot.eu>
2023-11-28 11:35:42 +0100

    refactor: parser with switch

--- lib/src/editable/parser.dart
+++ lib/src/editable/parser.dart
@@ -5,67 +5,73 @@ class GeneratedParser {
     final chars = txt.split('');
     if (chars.isEmpty) return null;
 
-    if(chars.first != '(') chars.insert(0, '(');
-    if(chars.last != ')') chars.add(')');
+    if (chars.first != '(') chars.insert(0, '(');
+    if (chars.last != ')') chars.add(')');
 
     EditableGen? current;
     final stack = <EditableGen>[];
 
     for (final i in chars) {
-      if (i == ' ') {
-        if (current is EditableTxt) {
-          final tmp = stack.last;
-          if (tmp is EditableCapsule) tmp.add(current);
-          if (tmp is EditableRandom) tmp.add(current);
-          current = stack.removeLast();
-        }
-      } else if (i == '(') {
-        if (current is EditableTxt) {
-          final tmp = stack.last;
-          if (tmp is EditableCapsule) tmp.add(current);
-          if (tmp is EditableRandom) tmp.add(current);
-          current = stack.removeLast();
-        }
-        if (current != null) stack.add(current);
-        current = EditableCapsule(encapsulated: []);
-      } else if (i == ')') {
-        if (current is EditableTxt) {
-          final tmp = stack.last;
-          if (tmp is EditableCapsule) tmp.add(current);
-          if (tmp is EditableRandom) tmp.add(current);
-          current = stack.removeLast();
-        }
-        if (stack.isNotEmpty && current != null) {
-          stack.last.add(current);
-          current = stack.removeLast();
-        }
-      } else if (i == '{') {
-        if (current is EditableTxt) {
-          final tmp = stack.last;
-          if (tmp is EditableCapsule) tmp.add(current);
-          if (tmp is EditableRandom) tmp.add(current);
-          current = stack.removeLast();
-        }
-        if (current != null) stack.add(current);
-        current = EditableRandom(possibilities: []);
-      } else if (i == '}') {
-        if (current is EditableTxt) {
-          final tmp = stack.last;
-          if (tmp is EditableCapsule) tmp.add(current);
-          if (tmp is EditableRandom) tmp.add(current);
-          current = stack.removeLast();
-        }
-        if (stack.isNotEmpty && current != null) {
-          stack.last.add(current);
-          current = stack.removeLast();
-        }
-      } else {
-        if (current is EditableTxt) {
-          current.add(EditableTxt(text: i));
-        } else {
+      switch (i) {
+        case ' ':
+          if (current is EditableTxt) {
+            final tmp = stack.last;
+            if (tmp is EditableCapsule) tmp.add(current);
+            if (tmp is EditableRandom) tmp.add(current);
+            current = stack.removeLast();
+          }
+          break;
+        case '(':
+          if (current is EditableTxt) {
+            final tmp = stack.last;
+            if (tmp is EditableCapsule) tmp.add(current);
+            if (tmp is EditableRandom) tmp.add(current);
+            current = stack.removeLast();
+          }
           if (current != null) stack.add(current);
-          current = EditableTxt(text: i);
-        }
+          current = EditableCapsule(encapsulated: []);
+          break;
+        case ')':
+          if (current is EditableTxt) {
+            final tmp = stack.last;
+            if (tmp is EditableCapsule) tmp.add(current);
+            if (tmp is EditableRandom) tmp.add(current);
+            current = stack.removeLast();
+          }
+          if (stack.isNotEmpty && current != null) {
+            stack.last.add(current);
+            current = stack.removeLast();
+          }
+          break;
+        case '{':
+          if (current is EditableTxt) {
+            final tmp = stack.last;
+            if (tmp is EditableCapsule) tmp.add(current);
+            if (tmp is EditableRandom) tmp.add(current);
+            current = stack.removeLast();
+          }
+          if (current != null) stack.add(current);
+          current = EditableRandom(possibilities: []);
+          break;
+        case '}':
+          if (current is EditableTxt) {
+            final tmp = stack.last;
+            if (tmp is EditableCapsule) tmp.add(current);
+            if (tmp is EditableRandom) tmp.add(current);
+            current = stack.removeLast();
+          }
+          if (stack.isNotEmpty && current != null) {
+            stack.last.add(current);
+            current = stack.removeLast();
+          }
+          break;
+        default:
+          if (current is EditableTxt) {
+            current.add(EditableTxt(text: i));
+          } else {
+            if (current != null) stack.add(current);
+            current = EditableTxt(text: i);
+          }
       }
     }
     return current;
